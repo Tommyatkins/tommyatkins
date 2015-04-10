e2edac597c1aa1708eec2701e9d79c85e9c78828
tommyatkins <tommyatkins@126.com>
2015-04-10 11:50:59 +0800

    jedis

---
+++ src/test/java/com/tommyatkins/test/redis/JedisClientTest.java
@@ -0,0 +1,35 @@
+package com.tommyatkins.test.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisClientTest {
+
+	private JedisPool jedisPool;
+	
+	{
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(200);
+		config.setMaxIdle(10);
+		config.setMaxWaitMillis(10000);
+		config.setTestOnBorrow(true);
+		jedisPool = new JedisPool(config, "127.0.0.1", 6379);
+	}
+	
+	public JedisPool getJedisPool() {
+		return this.jedisPool;
+	}
+	
+	public static void main(String[] args) {
+		JedisClientTest test = new JedisClientTest();
+		JedisPool pool = test.getJedisPool();
+		
+		Jedis conn = pool.getResource();
+		
+		pool.returnResource(conn);
+	}
+	
+	
+}
+
